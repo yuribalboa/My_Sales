dde91f3475b7831f210325d04a61f4ab7c4bb198
YuriTropeia <yurineudinetropeia@gmail.com>
2024-11-21 08:13:57 -0300

    feat: add gitignore

---
+++ src/modules/products/database/entities/Products.ts
@@ -0,0 +1,29 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('products')
+export class Product {
+@PrimaryGeneratedColumn()
+  id: string;
+
+  @Column({ type: 'text' })
+  name: string;
+
+  @Column({ type: 'decimal' })
+  price: number;
+
+  @Column({ type: 'int' })
+  quantity: number;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updated_at: Date;
+}
